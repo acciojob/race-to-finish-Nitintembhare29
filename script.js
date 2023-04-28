be680a5d7bdfabf0ad9a1f1a573315d9e1d9a20d
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-28 16:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-window.promises = [];
+window.promises = [
+	new Promise(resolve => setTimeout(() => resolve(1), Math.floor(Math.random() * 5000) + 1000)),
+			new Promise(resolve => setTimeout(() => resolve(2), Math.floor(Math.random() * 5000) + 1000)),
+			new Promise(resolve => setTimeout(() => resolve(3), Math.floor(Math.random() * 5000) + 1000)),
+			new Promise(resolve => setTimeout(() => resolve(4), Math.floor(Math.random() * 5000) + 1000)),
+			new Promise(resolve => setTimeout(() => resolve(5), Math.floor(Math.random() * 5000) + 1000)),
+];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+Promise.any(promises)
+			.then(result => {
+				document.getElementById("output").textContent = `The first resolved promise is ${result}`;
+			})
+			.catch(error => console.log(error));
